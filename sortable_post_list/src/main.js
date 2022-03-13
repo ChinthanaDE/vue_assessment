4e004f04d4bae45e0843fd64ab65cdd270d99a8c
= <chinthanadesilva2@gmail.com>
2022-03-13 12:51:10 +0530

    accordian componet created add fontawsome libary

--- sortable_post_list/src/main.js
+++ sortable_post_list/src/main.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import './assets/css/main.scss';
 
-createApp(App).use(store).use(router).mount('#app');
+library.add(faChevronDown, faChevronUp);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(store).use(router)
+  .mount('#app');
